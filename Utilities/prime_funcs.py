6980e742df78540d36b53bb70c6709717bc04618
James Triveri <james.triveri@gmail.com>
2017-11-26 20:42:17 -0600

    Added prime_generator function to Utilities.

--- Utilities/prime_funcs.py
+++ Utilities/prime_funcs.py
@@ -7,10 +7,11 @@ import math
 def is_prime(n):
     """Determine whether or not `n` is prime."""
     if n in (2, 3, 5, 7, 11, 13, 17, 19): return(True)
-    if (n<=1 or n%2==0 or n%3==0):        return(False)
+    if (n<=1 or n%2==0 or n%3==0): return(False)
     # determine upper limit of test range =>
     ulimit = (int(math.ceil(math.sqrt(n)))+1)
     return(not any(n%k==0 for k in range(3, ulimit, 2)))
+
 
 
 
@@ -25,8 +26,19 @@ def nth_prime(nth):
     # iterate until prime_cnt==nth =>
     prime_cnt = 4
     for iterval in itertools.count(11, 2):
-        itertst = is_prime(iterval)
-        prime_cnt+=(1 if itertst else 0)
+        prime_cnt+=(1 if is_prime(iterval) else 0)
         if prime_cnt==nth:
             res = iterval; break
     return(res)
+
+
+
+def prime_generator(start=1):
+    """
+    Prime number generator. First prime generated will be 5
+    (will not generate 2 & 3).
+    """
+    if start<1: return(None)
+    for i in itertools.count(start=start):
+        for j in  ((6 * i) - 1, (6 * i) + 1):
+            if is_prime(j): yield(j)
