8d7c0baf43a3f0d957c147fb2369ae4505a79f2e
jtrive84 <james.triveri@gmail.com>
2018-02-04 15:53:34 -0600

    Added Project_Euler_25.py.

--- Utilities/prime_funcs.py
+++ Utilities/prime_funcs.py
@@ -2,10 +2,10 @@ import itertools
 import math
 
 
-
-
-def is_prime(n):
-    """Determine whether or not `n` is prime."""
+def isprime(n):
+    """
+    Determine whether or not `n` is prime.
+    """
     if n in (2, 3, 5, 7, 11, 13, 17, 19): return(True)
     if (n<=1 or n%2==0 or n%3==0): return(False)
     # determine upper limit of test range =>
@@ -14,9 +14,10 @@ def is_prime(n):
 
 
 
-
 def nth_prime(nth):
-    """Return nth prime."""
+    """
+    Return nth prime.
+    """
     if nth<1:  return(None)
     if nth==1: return(2)
     if nth==2: return(3)
@@ -26,19 +27,37 @@ def nth_prime(nth):
     # iterate until prime_cnt==nth =>
     prime_cnt = 4
     for iterval in itertools.count(11, 2):
-        prime_cnt+=(1 if is_prime(iterval) else 0)
+        prime_cnt+=(1 if isprime(iterval) else 0)
         if prime_cnt==nth:
             res = iterval; break
     return(res)
 
 
 
-def prime_generator(start=1):
+def prime_stream(start=1):
     """
-    Prime number generator. First prime generated will be 5
-    (will not generate 2 & 3).
+    Prime number generator. First prime generated will
+    be 5 (will not generate 2 & 3).
     """
-    if start<1: return(None)
+    if start<1:
+        return(None)
     for i in itertools.count(start=start):
         for j in  ((6 * i) - 1, (6 * i) + 1):
-            if is_prime(j): yield(j)
+            if isprime(j):
+                yield(j)
+
+
+
+def primes_upto(n):
+    """
+    Return all primes numbers from 2 up to n.
+    """
+    # Initialize stream of primes.
+    plist   = [2, 3]
+    pstream = prime_stream()
+    iterp   = 3
+    while iterp<n:
+        iterp = next(pstream)
+        plist.append(iterp)
+    return(plist)
+    
